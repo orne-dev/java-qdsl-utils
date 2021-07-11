06e747f5c6853c4887cc06c03944c5e638089a48
(w) Iker Hernaez <wamphiry@orne.dev>
2021-07-11 11:56:33 +0200

    Update project configuration and dependencies

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,5 @@
 # :package: 0.1.0
 
 01. :gift: Initial version
+01. :wrench: Added `org.apache.commons:commons-lang3:3.12.0` dependency.
+01. :wrench: Added `com.querydsl:querydsl-core:5.0.0.M1` dependency.
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,2 +1,29 @@
+/*-
+ * #%L
+ * qdsl-utils
+ * %%
+ * Copyright (C) 2021 Orne Developments
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
 module dev.orne.qdsl {
+    exports dev.orne.qdsl;
+    requires org.slf4j;
+    requires org.apache.commons.lang3;
+    requires transitive com.querydsl.core;
+    requires static java.validation;
+    requires static org.apiguardian.api;
 }
